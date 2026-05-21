2ee04fd8fe8323b5dcd4d9d8e0db986265a3c6ea
Ramukvaraa <ramuk.varaa.to.cool@gmail.com>
2026-05-21 13:18:24 +0530

    Restyle game HUD for trash sort

--- game.js
+++
@@ -1,9 +0,0 @@
-// Compatibility shim for older links.
-// The real game script lives at js/game.js.
-(function loadRealGameScript() {
-  var script = document.createElement('script');
-  script.src = './js/game.js';
-  script.defer = true;
-  document.head.appendChild(script);
-})();
-
